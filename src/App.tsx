6a108c93ee100b5fd614cdc29fd3b54a8e3828c8
Christian Hesels <christianhesels@gmail.com>
2022-01-12 16:56:50 +0100

    Travis CI Branch test

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Seite wird momentan bearbeitet!</p>
+        <p>Ein Travis-CI Test</p>
       </header>
     </div>
   );
