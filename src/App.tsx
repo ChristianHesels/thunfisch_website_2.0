0dbabe4db1750fddd4a502669a29d7025261e861
Christian Hesels <christianhesels@gmail.com>
2022-01-12 16:29:17 +0100

    Added logo to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>Seite wird momentan bearbeitet!</p>
       </header>
     </div>
