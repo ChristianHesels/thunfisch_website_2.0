dec04d95486e9089f7f69e446814d57ed0f9eb8a
Christian Hesels <christianhesels@gmail.com>
2022-07-14 18:51:25 +0200

    Don't show YouTube Symbol without a link

--- src/components/TTTT/Discography.tsx
+++ src/components/TTTT/Discography.tsx
@@ -30,33 +30,39 @@ export default function Discography() {
               <div className="middle">
                 <div className="text">{album.name}</div>
               </div>
-              <a href={album.youtube} target="_blank">
-                <span
-                  className="iconify"
-                  data-icon="akar-icons:youtube-fill"
-                  style={{
-                    color: '#FF0000',
-                  }}
-                />
-              </a>
-              <a href={album.spotify} target="_blank">
-                <span
-                  className="iconify"
-                  data-icon="mdi:spotify"
-                  style={{
-                    color: '#1DB954',
-                  }}
-                />
-              </a>
-              <a href={album.apple} target="_blank">
-                <span
-                  className="iconify"
-                  data-icon="cib:apple-music"
-                  style={{
-                    color: '#f94c57',
-                  }}
-                />
-              </a>
+              {album.youtube && (
+                <a href={album.youtube} target="_blank">
+                  <span
+                    className="iconify"
+                    data-icon="akar-icons:youtube-fill"
+                    style={{
+                      color: '#FF0000',
+                    }}
+                  />
+                </a>
+              )}
+              {album.spotify && (
+                <a href={album.spotify} target="_blank">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:spotify"
+                    style={{
+                      color: '#1DB954',
+                    }}
+                  />
+                </a>
+              )}
+              {album.apple && (
+                <a href={album.apple} target="_blank">
+                  <span
+                    className="iconify"
+                    data-icon="cib:apple-music"
+                    style={{
+                      color: '#f94c57',
+                    }}
+                  />
+                </a>
+              )}
             </div>
           </Grid>
         ))}
@@ -83,24 +89,28 @@ export default function Discography() {
                 <div className="middle">
                   <div className="text">{song.name}</div>
                 </div>
-                <a href={song.youtube} target="_blank">
-                  <span
-                    className="iconify"
-                    data-icon="akar-icons:youtube-fill"
-                    style={{
-                      color: '#FF0000',
-                    }}
-                  />
-                </a>
-                <a href={song.spotify} target="_blank">
-                  <span
-                    className="iconify"
-                    data-icon="mdi:spotify"
-                    style={{
-                      color: '#1DB954',
-                    }}
-                  />
-                </a>
+                {song.youtube && (
+                  <a href={song.youtube} target="_blank">
+                    <span
+                      className="iconify"
+                      data-icon="akar-icons:youtube-fill"
+                      style={{
+                        color: '#FF0000',
+                      }}
+                    />
+                  </a>
+                )}
+                {song.spotify && (
+                  <a href={song.spotify} target="_blank">
+                    <span
+                      className="iconify"
+                      data-icon="mdi:spotify"
+                      style={{
+                        color: '#1DB954',
+                      }}
+                    />
+                  </a>
+                )}
                 <a href={song.apple} target="_blank">
                   <span
                     className="iconify"
